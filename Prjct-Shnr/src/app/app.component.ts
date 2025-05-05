300c62b9271a17216fc0882269adaebc66ecf008
alex-bachir <alex.bachir@laplateforme.io>
2025-05-05 16:13:30 +0200

    header fait

--- Prjct-Shnr/src/app/app.component.ts
+++ Prjct-Shnr/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<h1>Bienvenue</h1>',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `<nav class="navbar">
+  <div class="logo">
+    <img src="StudioShinra.png" alt="Logo" />
+  </div>
+  <div class="nav-links">
+    <a href="#">Mon Compte</a>
+    <a href="#">Article</a>
+  </div>
+</nav>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
