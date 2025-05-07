a72f7175dc4c3da26edb12d48be013f81d5b968e
alex-bachir <alex.bachir@laplateforme.io>
2025-05-07 23:23:19 +0200

    ajout de la barre de recherche et du bouton catalogue dans app.component.ts & app.component.css

--- Prjct-Shnr/src/app/app.component.ts
+++ Prjct-Shnr/src/app/app.component.ts
@@ -5,15 +5,29 @@ import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent],
-  template: `<nav class="navbar">
-  <div class="logo">
-    <img src="StudioShinra.png" alt="Logo" />
-  </div>
-  <div class="nav-links">
-    <a href="#">Mon Compte</a>
-    <a href="#">Article</a>
-  </div>
-</nav>`,
+  template: `
+  <nav class="navbar">
+    <div class="logo">
+      <img src="StudioShinra.png" alt="Logo" />
+    </div>
+    <div class="nav-links">
+      <a href="#">Mon Compte</a>
+      <a href="#">Article</a>
+    </div>
+    
+  </nav>
+  <nav class ="navbar2">
+    <div class="search-bar">
+      <form action="/rechercher" method="GET">
+        <input type="text" name="q" placeholder="Rechercher">
+      </form>
+    </div>
+    <div class = "btn">
+      <button class="catalogue-button">
+        Catalogue
+      </button>
+    </div>
+  </nav>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
