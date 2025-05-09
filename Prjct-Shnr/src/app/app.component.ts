9fd5eb86878c40c1e8faf45831dd86f682866e39
alex-bachir <alex.bachir@laplateforme.io>
2025-05-09 21:28:33 +0200

    modfication footer html et css

--- Prjct-Shnr/src/app/app.component.ts
+++ Prjct-Shnr/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent, FooterComponent],
+  imports: [ HeaderComponent, FooterComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
