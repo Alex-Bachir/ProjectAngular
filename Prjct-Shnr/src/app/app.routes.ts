81e3c830f6e82a5e3a21d6c06c32ccb4660ced35
alex-bachir <alex.bachir@laplateforme.io>
2025-05-13 21:15:21 +0200

    ajout d'une path dans app.routes.ts

--- Prjct-Shnr/src/app/app.routes.ts
+++ Prjct-Shnr/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-
+import { ResultatComponent } from './resultat/resultat.component';
 
 export const routes: Routes = [
     
+    { path: 'resultat', component: ResultatComponent},
 
 ];
